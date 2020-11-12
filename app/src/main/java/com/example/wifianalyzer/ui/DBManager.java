c8ee5c61ca515e7fb098562dc0cc22438c931e5d
Varun Pattar <pattarvarun.171co249@nitk.edu.in>
2020-11-12 22:40:05 +0530

    added ui

---
+++ app/src/main/java/com/example/wifianalyzer/ui/DBManager.java
@@ -0,0 +1,28 @@
+package com.example.wifianalyzer.ui;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBManager {
+    private Context context;
+    private SQLiteDatabase database;
+    private SampleSQLiteDBHelper dbHelper;
+
+    public DBManager(Context c) {
+        this.context = c;
+    }
+
+    public DBManager open() throws SQLException {
+        this.dbHelper = new SampleSQLiteDBHelper(this.context);
+        this.database = this.dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        this.dbHelper.close();
+    }
+
+
